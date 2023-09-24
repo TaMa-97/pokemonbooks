485a93959714d7522eea6e30ec7e73ce518a2177
Lilly <myk19977@gmail.com>
2023-09-24 13:55:19 +0900

    add: pokeapi

--- src/App.js
+++ src/App.js
@@ -1,52 +1,61 @@
-import "./App.css";
-import "./index.css";
+import { useEffect, useState } from "react";
 import PokemonThumbnails from "./PokemonThumbnails";
 
+import "./App.css";
+import "./index.css";
+
 function App() {
-  const pokemon = [
+  const [pokemonNames, setPokemonNames] = useState([]);
+
+  const pokemons = [
     {
       id: 1,
-      name: "フシギダネ",
       image:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
       type: "くさ",
     },
     {
       id: 2,
-      name: "フシギソウ",
       image:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
       type: "くさ",
     },
     {
       id: 3,
-      name: "フシギバナ",
       image:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
       type: "くさ",
     },
   ];
 
+  const url = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results[0].name);
+        // 仮で３つのポケモンの名前をセットする
+        const names = [
+          data.results[0].name,
+          data.results[1].name,
+          data.results[2].name,
+        ];
+        setPokemonNames(names);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <PokemonThumbnails
-        id={pokemon[0].id}
-        name={pokemon[0].name}
-        image={pokemon[0].image}
-        type={pokemon[0].type}
-      />
-      <PokemonThumbnails
-        id={pokemon[1].id}
-        name={pokemon[1].name}
-        image={pokemon[1].image}
-        type={pokemon[1].type}
-      />
-      <PokemonThumbnails
-        id={pokemon[2].id}
-        name={pokemon[2].name}
-        image={pokemon[2].image}
-        type={pokemon[2].type}
-      />
+      {pokemons.map((pokemon, index) => (
+        <PokemonThumbnails
+          id={pokemon.id}
+          name={pokemonNames[index]}
+          image={pokemon.image}
+          type={pokemon.type}
+          key={index}
+        />
+      ))}
     </div>
   );
 }
